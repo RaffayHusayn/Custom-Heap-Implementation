1abbfd83ec60b3570cf2e1299de6f9b6ee906397
Raffay Syed <raffayhusayn@gmail.com>
2022-02-28 03:05:41 -0500

    Add functionality to pop the head of the heap

--- src/com/fclass/Main.java
+++ src/com/fclass/Main.java
@@ -4,15 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        MaxHeap heap = new MaxHeap(4);
+        MaxHeap heap = new MaxHeap(10);
         heap.add(3);
-        heap.add(6);
-        heap.add(0);
-        heap.add(4);
+//        heap.add(10);
+//        heap.add(11);
+//        heap.add(12);
+//        heap.add(39);
+//        heap.add(67);
+//        heap.add(0);
+//        heap.add(4);
         int[] values = heap.values();
         for(int i = 0 ; i < values.length; i++){
             System.out.println(values[i]);
         }
-//        heap.insert(2);
+        System.out.println("poping this out");
+        System.out.println(heap.pop());
+        System.out.println("remianing tree");
+        values = heap.values();
+        for(int i = 0 ; i < values.length; i++){
+            System.out.println(values[i]);
+        }
+        heap.add(2);
+        System.out.println(heap.pop());
     }
 }
--- src/com/fclass/MaxHeap.java
+++ src/com/fclass/MaxHeap.java
@@ -15,6 +15,7 @@ public class MaxHeap {
 
     //inserting elements
     public void add(int num){
+
         //don't wanna store anything on the 0th index, so that we can use it for other stuff
         currentSize++;
         if(currentSize >= heapSize){
@@ -27,17 +28,6 @@ public class MaxHeap {
 
         //polling the heap
         siftup(currentSize);
-//        int index = currentSize;
-//        int parent = index/2; //so 2,3 are child of 1 -> 2/2= 1 and 3/2=1
-//        //currentsize > 1 because if currentSize =1 then it means it's the first element and can't have parent
-//        while (maxHeap[index] > maxHeap[parent] && index > 1){
-//            //replace maxHeap[parent] with maxHeap[currentSize]
-//            int temp = maxHeap[parent];
-//            maxHeap[parent] = maxHeap[index];
-//            maxHeap[index] = temp;
-//            index = parent;
-//            parent = parent/2;
-//        }
     }
     private void siftup(int index){
 
@@ -52,12 +42,48 @@ public class MaxHeap {
             parent = parent/2;
         }
     }
+    /*
+                           1
+                  2              3
+             4        5       6        7
+           8   9    10  11  12  13   14  15
+     */
+    private void siftdown(){
+        int replaceIndex;
+        int parent = 1;
+        while(parent <= currentSize / 2 ){
+            int left = parent * 2;
+            int right = parent * 2 + 1;
+            if (maxHeap[left] > maxHeap[right]){
+                replaceIndex = left;
+            }else{
+                replaceIndex = right;
+            }
+
+            if(maxHeap[parent] < maxHeap[replaceIndex]){
+                int temp = maxHeap[parent];
+                maxHeap[parent] = maxHeap[replaceIndex];
+                maxHeap[replaceIndex] = temp;
+
+                parent = replaceIndex;
+
+                }
+            }
+
+    }
     public int peek(){
         return maxHeap[1];
     }
 
-    public int[] values(){
-        return Arrays.stream(maxHeap).skip(1).toArray();//skip the first one from maxHeap
+    public int pop(){
+        if(currentSize < 1 ){
+            throw new ArrayIndexOutOfBoundsException("there's no element in the heap to remove");
+        }
+        int temp = maxHeap[1];
+        maxHeap[1] = maxHeap[currentSize]; //because the first element of array is always empty so currentSize-1 is not gonna work
+        currentSize--;
+        siftdown();
+        return temp;
     }
 
 }
